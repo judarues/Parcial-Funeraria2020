4d0f37a217270b69c066eb108e98c8e927c78df6
Juan David Ruiz <judarues@gmail.com>
2020-10-08 23:09:02 -0500

    Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewHomeComponent } from './view-home/view-home.component';
+import { ComponentMenuComponent } from './component-menu/component-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewHomeComponent,
+    ComponentMenuComponent
   ],
   imports: [
     BrowserModule,
